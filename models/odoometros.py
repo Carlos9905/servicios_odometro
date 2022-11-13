adb934b0b832e860a40056d72fbc12420390a769
Carlos0599 <joseaguilar0522@gmail.com>
2022-11-12 21:03:01 -0600

    Cambio

--- models/odoometros.py
+++ models/odoometros.py
@@ -13,18 +13,19 @@ class Odometros(models.Model):
     unidad = fields.Many2one("fleet.vehicle", string="Unidad", required=True)
     costo = fields.Float(string="Costo")
     fecha = fields.Date(string="Fecha")
-    tipo_carga = fields.Many2one(
-        string="Tipo de carga",
-        comodel_name="fleet.vehicle.log.services"
+    tipo_carga = fields.Selection(
+        string="Tipo Carga",
+        selection=[("servicio", "Servicio"), ("combustible", "Combustible")],
     )
+    _tipo_carga = fields.Many2one("fleet.vehicle.log.services", string="Tipo Carga")
     odo_inicial = fields.Float(
         string="Odomtro Inicial", compute="_calculo_odoInicial", store=True
     )
-    odo_final = fields.Float(string="Odometro Final", default=1.0)
+    odo_final = fields.Float(string="Odometro Final")
     litros = fields.Float(string="Litros")
     km_acumulado = fields.Float(string="Kilomtros acumuladsos")
     ren_optimo = fields.Float(string="Rendimiento Optimo")
-
+    
     @api.model
     def create(self, vals):
         if vals.get("name", ("New")) == ("New"):
@@ -43,36 +44,30 @@ class Odometros(models.Model):
 
     @api.onchange("odo_final")
     def _registrar(self):
-        for record in self:
-            if record.odo_final <= 0.0:
-                raise ValidationError(
-                    "El Valor del odometro final no puede ser menor ó igual 0"
-                )
-            else:
-                if record.tipo_carga:
-                    registros = self.env["km.finales"]
-                    registros.create(
-                        {
-                            "tipo": "servicio",
-                            "odo_final": record.odo_final,
-                            "unidad": record.unidad.license_plate,
-                        }
+        if self.odo_final > 0:
+            for record in self:
+                if record.odo_final <= 0.0:
+                    raise ValidationError(
+                        "El Valor del odometro final no puede ser menor ó igual 0"
                     )
                 else:
                     registros = self.env["km.finales"]
                     registros.create(
                         {
-                            "tipo": "combustible",
+                            "tipo": "servicio" if record._tipo_carga else "combustible",
                             "odo_final": record.odo_final,
                             "unidad": record.unidad.license_plate,
+                            "servicio": record.name
                         }
                     )
+        else:
+            pass
 
     @api.depends("odo_final")
     def _calculo_odoInicial(self):
         for record in self:
             registros = self.env["km.finales"].search(
-                [("tipo", "=", record.tipo_carga)]
+                [("tipo", "=", "servicio" if record._tipo_carga else "combustible")]
             )
             filtro = registros.filtered(
                 lambda f: f.odo_final > 0 and f.unidad == record.unidad.license_plate
@@ -85,6 +80,16 @@ class Odometros(models.Model):
             else:
                 record.odo_inicial = record.odo_final
 
+    @api.ondelete(at_uninstall=False)
+    def delete_record(self):
+        registros = self.env["km.finales"].search([])
+        filtro = registros.filtered(lambda variable: variable.servicio)
+        #mapeo = filtro.mapped("servicio")
+        for record in self:
+            if record.name == filtro.servicio:
+                filtro.servicio.unlink()
+
+            
 
 class KmFinales(models.Model):
     _name = "km.finales"
@@ -92,6 +97,4 @@ class KmFinales(models.Model):
     tipo = fields.Char("Tipo")
     odo_final = fields.Float("Odometro final")
     unidad = fields.Char("Unidad")
-    servicio_id = fields.Many2one(
-        "servicios.odometros", string="Servicio al que pertence"
-    )
+    servicio = fields.Char("Servicio al que pertenece")
